e73d57a48f3b5ced8a67f043ed4b768c1121d924
scd7896 <scd1212@naver.com>
2022-07-10 17:38:28 +0900

    fix: turn 변경함에 따라 카메라 회전 시키도록함

--- src/entities/CameraEntity.ts
+++ src/entities/CameraEntity.ts
@@ -1,24 +1,59 @@
 import ChessSystem from "../system/ChessSystem";
+import { Team } from "../types";
 import Entity from "./Entity";
 
 class CameraEntity extends Entity<ChessSystem> {
   private _tick: number;
+  private _isRoation: boolean;
+  private _turn: Team;
 
   start(): void {}
 
   update(time: number): void {
+    if (this._isRoation) this.rotationCamera(time);
+  }
+
+  rotationCamera(time: number) {
     this._tick = (this._tick + time / 10) % 360;
     const pi = (this._tick / 180) * Math.PI;
     const z = this._getCameraZ(pi);
     const x = this._getCameraX(z);
+
     const isReverse = this._tick >= 180 && this._tick <= 360;
     this.system.camera.position.set(isReverse ? -1 * x : x, this.system.camera.position.y, z);
     this.system.camera.lookAt(0, 0, 0);
+    this._checkRotation(this._tick);
+  }
+
+  changeTurn() {
+    if (this._turn === "white") this._turn = "black";
+    else if (this._turn === "black") this._turn = "white";
+    this._isRoation = true;
   }
 
   constructor(system: ChessSystem) {
     super(system);
     this._tick = 0;
+    this._isRoation = false;
+    this._turn = "white";
+  }
+
+  private _checkRotation(tick: number) {
+    if (this._turn === "white") {
+      if (Math.abs(tick - 0) < 5 || Math.abs(tick - 360) < 5) {
+        this._isRoation = false;
+        this.system.camera.position.set(0, this.system.camera.position.y, 40);
+        this.system.camera.lookAt(0, 0, 0);
+      }
+    }
+
+    if (this._turn === "black") {
+      if (Math.abs(tick - 180) < 5) {
+        this._isRoation = false;
+        this.system.camera.position.set(0, this.system.camera.position.y, -40);
+        this.system.camera.lookAt(0, 0, 0);
+      }
+    }
   }
 
   private _getCameraZ(pi: number) {
