f01838e14cc5b2021ac49b0b7db0c3a4fd1dae06
scd7896 <scd1212@naver.com>
2022-07-12 21:49:05 +0900

    fix: knight 움직임 구현

---
+++ src/components/Unit/Knight.ts
@@ -0,0 +1,49 @@
+import ChessSystem from "../../system/ChessSystem";
+import { Position, Team } from "../../types";
+import Unit from "./Unit";
+
+class Knight extends Unit {
+  constructor(team: Team, defaultPosition: Position, system: ChessSystem) {
+    super(team, "knight", defaultPosition, system);
+  }
+  getCanMovePositions(): Position[] {
+    return [
+      ...this._getMovePositions("row", true),
+      ...this._getMovePositions("row", false),
+      ...this._getMovePositions("col", true),
+      ...this._getMovePositions("col", false),
+    ];
+  }
+
+  private _getIsMove(position: Position) {
+    const unit = this.system.getUnitByPosition(position)[0];
+    if (!unit) return true;
+    if (unit.team === this.team) return false;
+    return true;
+  }
+
+  private _getMovePositions(type: "col" | "row", isUp: boolean): Position[] {
+    const result: Position[] = [];
+    const diff = isUp ? 2 : -2;
+    const base = type === "row" ? this.position[0] + diff : this.position[1] + diff;
+    const left = type === "row" ? this.position[1] - 1 : this.position[0] - 1;
+    const right = type === "row" ? this.position[1] + 1 : this.position[0] + 1;
+
+    if (base >= 0 && base < 8) {
+      if (left >= 0 && left < 8) {
+        const row = type === "row" ? base : left;
+        const col = type === "row" ? left : base;
+        if (this._getIsMove([row, col])) result.push([row, col]);
+      }
+      if (right >= 0 && right < 8) {
+        const row = type === "row" ? base : right;
+        const col = type === "row" ? right : base;
+        if (this._getIsMove([row, col])) result.push([row, col]);
+      }
+    }
+
+    return result;
+  }
+}
+
+export default Knight;
--- src/entities/chessPiece/KnightEntity.ts
+++ src/entities/chessPiece/KnightEntity.ts
@@ -1,26 +1,12 @@
-import Pawn from "../../components/Unit/Pawn";
+import Knight from "../../components/Unit/Knight";
 import ChessSystem from "../../system/ChessSystem";
-import { IChess, Position } from "../../types";
+import { IChess } from "../../types";
 import { ChessUnitEntity } from "../Entity";
 
 class KnightEntity extends ChessUnitEntity {
-  move(position: Position): void {
-    throw new Error("Method not implemented.");
-  }
-  select(): void {
-    // throw new Error("Method not implemented.");
-  }
-
-  start(): void {
-    this.loadUnit();
-  }
-  update(delta: number): void {
-    // throw new Error("Method not implemented.");
-  }
-
   constructor(chess: IChess, system: ChessSystem) {
     super(chess, system);
-    this._unitComponent = new Pawn(chess.team, chess.position, system);
+    this._unitComponent = new Knight(chess.team, chess.position, system);
   }
 }
 
